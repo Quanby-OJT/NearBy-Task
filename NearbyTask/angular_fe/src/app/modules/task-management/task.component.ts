a55240b35b3cc8e5fb63ebf571e8ceea94b3e87b
EdersonMiraflor <125487899+EdersonMiraflor@users.noreply.github.com>
2025-02-27 08:24:50 +0800

    Created Structure of Form again
    Created Structure of Form again which can now crud to be implemented

--- NearbyTask/angular_fe/src/app/modules/task-management/task.component.ts
+++ NearbyTask/angular_fe/src/app/modules/task-management/task.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { TaskReportedListComponent } from './task-reported-list/task-reported-list.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskService } from '../services/task.service'; // Import TaskService
 
 @Component({
   selector: 'app-task',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    AngularSvgIconModule,
+    TaskListComponent,
+    TaskReportedListComponent,
+  ],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css',
+  styleUrl: './task.component.css'
 })
-export class TaskComponent {}
+export class TaskComponent implements OnInit {
+  currentTab: string = "showTaskList";
+  tasks: any[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit() {
+    this.loadTasks();
+  }
+
+  loadTasks() {
+    this.taskService.getTasks().subscribe({
+      next: (response) => {
+        this.tasks = response.tasks; // Assuming API returns { tasks: [...] }
+      },
+      error: (error) => {
+        console.error("Error fetching tasks:", error);
+      }
+    });
+  }
+
+  changeTab(tabName: string) {
+    this.currentTab = tabName;
+  }
+}
