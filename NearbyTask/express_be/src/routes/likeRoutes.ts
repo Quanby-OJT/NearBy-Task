ca144527ced1eec383b6e4202c9b2b014e4d7eb9
Ronnie F. Estillero || BSIT-2B <124552782+Roo0nnie@users.noreply.github.com>
2025-03-03 14:29:56 +0800

    Update likeRoutes.ts

--- NearbyTask/express_be/src/routes/likeRoutes.ts
+++ NearbyTask/express_be/src/routes/likeRoutes.ts
@@ -1,9 +1,1 @@
-import { Router } from "express";
-import LikeController from "../controllers/likeController";
 
-
-const router = Router();
-
-router.post("/likeJob", LikeController.createLike);
-
-export default router;
