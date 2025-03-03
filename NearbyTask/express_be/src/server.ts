041d76018cdcdf97002db4a74aa5196ee0cd215f
Ronnie F. Estillero || BSIT-2B <124552782+Roo0nnie@users.noreply.github.com>
2025-03-03 14:25:43 +0800

    Update server.ts

--- NearbyTask/express_be/src/server.ts
+++ NearbyTask/express_be/src/server.ts
@@ -1,45 +1,33 @@
 import express, { Application } from "express";
 import cors from "cors";
-import { port, session_key } from "./config/configuration";
+import { port } from "./config/configuration";
 import server from "./routes/apiRoutes";
 import userRoute from "./routes/userRoutes";
 import userAccountRoute from "./routes/userAccountRoutes";
 import taskRoutes from "./routes/taskRoutes";
 import dotenv from "dotenv";
-
 import authRoutes from "./routes/authRoutes";
-
-import session from "express-session"
-import likeRoutes from "./routes/likeRoutes";
 
 dotenv.config();
 const app: Application = express();
 
-declare module 'express-session' {
-  interface SessionData {
-    user: string;
-  }
-}
-
 // Middleware
-app.use(cors({
-  origin: "http://10.0.2.2:5000", // Replace with your frontend URL
-  credentials: true, // Allow creden
-}));
+app.use(cors());
 app.use(express.json());
-app.use(session({ 
-  secret: session_key,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {maxAge: 1000 * 60 * 60} //1 hour only
-}))
 
 // Routes
-app.use("/connect", server);
+app.use(
+  "/connect",
+  server,
+  userAccountRoute,
+  userRoute,
+  taskRoutes,
+  authRoutes
+);
 
 // Start server
 const PORT = port || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  console.log("Click this to direct: http://localhost:" + PORT +  "/connect");
+  console.log("Click this to direct: http://localhost:" + PORT + "/connect");
 });
