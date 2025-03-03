e5e0b998d155dc04dae348d38fbc670e4574ddd3
Ronnie F. Estillero || BSIT-2B <124552782+Roo0nnie@users.noreply.github.com>
2025-03-03 14:30:11 +0800

    Update authRoutes.ts

--- NearbyTask/express_be/src/routes/authRoutes.ts
+++ NearbyTask/express_be/src/routes/authRoutes.ts
@@ -1,14 +1,1 @@
-import { Router } from "express";
-import AuthenticationController from "../controllers/authenticationController";
-import { validateLogin, validateOTP } from "../validator/authValidator";
-import { handleValidationErrors } from "../middleware/validationMiddleware";
-import auth from "../controllers/authAngularController";
-const router = Router();
 
-/** Authentication Routes */
-
-router.post("/login", auth.login);
-router.post("/logout", auth.logout);
-router.post("/logout-without-session", auth.logoutWithoutSession);
-
-export default router;
